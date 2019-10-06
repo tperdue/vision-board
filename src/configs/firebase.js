8c94af9350cb67bda58ac1c70555ac4ab9cdb81e
Timothy Perdue <tp.webdev@gmail.com>
2019-10-06 12:02:27 -0400

    Adding firebase

---
+++ src/configs/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+
+
+const config = {
+    apiKey: "AIzaSyCRPovKA1wK72puzf_ia8nDeXmS6EeQKps",
+    authDomain: "vision-board-51991.firebaseapp.com",
+    databaseURL: "https://vision-board-51991.firebaseio.com",
+    projectId: "vision-board-51991",
+    storageBucket: "vision-board-51991.appspot.com",
+    messagingSenderId: "818423804592",
+    appId: "1:818423804592:web:de12cc924cec4974ad8175",
+    measurementId: "G-6FM29G66GY"
+};
+
+
+firebase.initializeApp(config);
