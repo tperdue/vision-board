64b1e19694e82937af654942c3778acb574f708e
Timothy Perdue <tp.webdev@gmail.com>
2019-10-18 10:02:47 -0400

    setting url value of selected canvas

--- src/reena-compononents/Addphoto.js
+++ src/reena-compononents/Addphoto.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import { Widget} from "@uploadcare/react-widget";
-import { connect }from 'react-redux';
+import { Widget } from "@uploadcare/react-widget";
+import { connect } from 'react-redux';
 import uploadcareTabEffects from 'uploadcare-widget-tab-effects'
 
 
 
 
-const AddPhoto = (props)=>{
-    const widgetApi = React.useRef();
-    console.log(uploadcareTabEffects)
+const AddPhoto = (props) => {
+    const editTab = (container, button, ...theArgs) => {
+        button[0].textContent = 'Edit Photo';
+        return uploadcareTabEffects(container, button, ...theArgs);
+    }
 
-    
+
 
     return (<div >
         <p>
-        <label htmlFor='file'>Your file:</label>{' '}
-        <Widget 
-            publicKey='512c413de32b68f92c92' 
-            id='file' 
-            onChange={(info)=>{console.log(info); props.addImage(info.cdnUrl)}}
-            customTabs={{"Effects": uploadcareTabEffects}}
-            tabs="file Effects"
-            effects='crop, rotate, mirror, flip, blur, sharp, enhance, grayscale, invert'
-            
+            <label htmlFor='file'>Your file:</label>{' '}
+            <Widget
+                publicKey='512c413de32b68f92c92'
+                id='file'
+                onChange={(info) => { console.log(info); props.addImage(info.cdnUrl) }}
+                customTabs={{ "Effects": uploadcareTabEffects }}
+                tabs="file Effects"
+                effects='crop, rotate, mirror, flip, blur, sharp, enhance, grayscale, invert'
+
 
             />
         </p>
     </div>)
 }
 
+
+
 const matchDispatchToProps = (dispatch) => {
     return {
-        addImage: (image)=>dispatch({type:'ADD_IMAGE', payload: image }),
+        addImage: (image) => dispatch({ type: 'ADD_IMAGE', payload: image }),
     }
 }
 
