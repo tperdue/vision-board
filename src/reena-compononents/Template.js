bb61109c9f0c604e61734f0a36dc47d807c49a63
LeMichael07 <lemichael07@gmail.com>
2019-10-24 21:10:17 -0400

    removed Canvas margin prop

--- src/reena-compononents/Template.js
+++ src/reena-compononents/Template.js
@@ -34,7 +34,7 @@ class Template extends Component {
                 border={canvasObj.border}
                 color={canvasObj.color}
                 radius={canvasObj.radius}
-                margin={canvasObj.margin}
+                // margin={canvasObj.margin}
                 url={canvasObj.selected && this.props.selectedItem ? this.props.selectedItem.webformatURL : canvasObj.url}
                 key={canvasObj.id}
                 clicked={() => { this.props.clicked(canvasObj.id) }} />)
